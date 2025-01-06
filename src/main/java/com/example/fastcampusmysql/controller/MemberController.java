c29a6cca71878ae9aa5dcbd888d6f204e495134f
SeongRyong Lim <ryongseong.dev@gmail.com>
2025-01-06 16:43:39 +0900

    ADD : 회원 이름 변경 내역 저장 구현

--- src/main/java/com/example/fastcampusmysql/controller/MemberController.java
+++ src/main/java/com/example/fastcampusmysql/controller/MemberController.java
@@ -1,12 +1,15 @@
 package com.example.fastcampusmysql.controller;
 
 import com.example.fastcampusmysql.domain.member.dto.MemberDto;
+import com.example.fastcampusmysql.domain.member.dto.MemberNicknameHistoryDto;
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
 import com.example.fastcampusmysql.domain.member.service.MemberReadService;
 import com.example.fastcampusmysql.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,12 +19,23 @@ public class MemberController {
 
     @PostMapping("/members")
     public MemberDto register(@RequestBody RegisterMemberCommand command) {
-        var member = memberWriteService.register(command);
+        var member = memberWriteService.create(command);
         return memberReadService.toDto(member);
     }
 
     @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable Long id) {
         return memberReadService.getMember(id);
+    }
+
+    @PostMapping("/{id}/name")
+    public MemberDto changeNickname(@PathVariable Long id, @RequestBody String nickname) {
+        memberWriteService.changeNickname(id, nickname);
+        return memberReadService.getMember(id);
+    }
+
+    @GetMapping("/{memberId}/nickname-histories")
+    public List<MemberNicknameHistoryDto> getNicknameHistories(@PathVariable Long memberId) {
+        return memberReadService.getNicknameHistories(memberId);
     }
 }
