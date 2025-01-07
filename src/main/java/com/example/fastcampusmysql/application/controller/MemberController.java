2967472ae850fc2842cdb00cd5fa1a3f3796ce02
SeongRyong Lim <ryongseong.dev@gmail.com>
2025-01-07 21:59:14 +0900

    ADD : 팔로우 등록, 조회 구현

--- src/main/java/com/example/fastcampusmysql/application/controller/MemberController.java
+++ src/main/java/com/example/fastcampusmysql/application/controller/MemberController.java
@@ -1,4 +1,4 @@
-package com.example.fastcampusmysql.controller;
+package com.example.fastcampusmysql.application.controller;
 
 import com.example.fastcampusmysql.domain.member.dto.MemberDto;
 import com.example.fastcampusmysql.domain.member.dto.MemberNicknameHistoryDto;
@@ -7,7 +7,12 @@ import com.example.fastcampusmysql.domain.member.entity.Member;
 import com.example.fastcampusmysql.domain.member.service.MemberReadService;
 import com.example.fastcampusmysql.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
