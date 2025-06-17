820b3fb052fc2f0188a70301bdaa1810ac5d02a2
ryongseong <ryongseong.dev@gmail.com>
2025-06-17 20:44:52 +0900

    Feat : 엔티티 회원 이름 변경 및 단위 테스트

--- src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
@@ -27,14 +27,20 @@ public class Member {
         this.id = id;
         this.email = Objects.requireNonNull(email);
         this.birthday = Objects.requireNonNull(birthday);
-        
+
         validateNickname(nickname);
         this.nickname = Objects.requireNonNull(nickname);
 
         this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
     }
 
-    void validateNickname(String nickname) {
+    public void changeNickname(String to) {
+        Objects.requireNonNull(to);
+        validateNickname(to);
+        nickname = to;
+    }
+
+    private void validateNickname(String nickname) {
         Assert.isTrue(nickname.length() <= NAME_MAX_LENGTH, "닉네임은 10자를 넘길 수 없습니다.");
     }
 }
